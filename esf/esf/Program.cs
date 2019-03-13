0eb7e7178ae91f3cd644e692ef3b9bc602c09c46
Bagó Gergő <bagogergo0@gmail.com>
2019-03-13 12:04:43 +0100

    #4:Ciklusok

--- esf/esf/Program.cs
+++ esf/esf/Program.cs
@@ -10,6 +10,10 @@ namespace esf
     {
         static void Main(string[] args)
         {
+            int v = 0;
+
+
+
             //A létrehozott  új típussal inicializálunk egy objektumot
             Exercise ex = new Exercise("Bagó Gergő");
 
@@ -22,7 +26,31 @@ namespace esf
             //Tagfügvény meghívása
             ex.PrintName();
 
+            //Ezt kellene megoldani
             Console.WriteLine(ex.getName());
+
+            ///Ciklusok
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Ciklus:<Változó hivatkozás I.>" + i);
+                Console.WriteLine("Ciklus: {0}", i);
+            }
+
+            v = 0;
+            while (v>0)
+            {
+                Console.WriteLine("While ciklus");
+                v--;
+            }
+
+            //Do-while: Hátultesztelős ciklus
+            v = 2;
+            do
+            {
+                Console.WriteLine("Do While Ciklus");
+                v--;
+            } while (v>0);
 
             //Beolvas egy karaktert a konzolról
             Console.ReadKey();
